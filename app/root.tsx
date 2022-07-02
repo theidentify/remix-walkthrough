19fa5ad673c3fbd03383954b52822c060a843764
theidentify <31946405+theidentify@users.noreply.github.com>
2022-07-02 13:09:00 +0700

    error boundary

--- app/root.tsx
+++ app/root.tsx
@@ -1,45 +1,66 @@
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Outlet,
-} from "@remix-run/react";
+import type { LinksFunction } from '@remix-run/node';
+import { Links, LiveReload, Outlet } from '@remix-run/react';
 
-import globalStylesUrl from "./styles/global.css";
-import globalMediumStylesUrl from "./styles/global-medium.css";
-import globalLargeStylesUrl from "./styles/global-large.css";
+import globalStylesUrl from './styles/global.css';
+import globalMediumStylesUrl from './styles/global-medium.css';
+import globalLargeStylesUrl from './styles/global-large.css';
 
 export const links: LinksFunction = () => {
   return [
     {
-      rel: "stylesheet",
+      rel: 'stylesheet',
       href: globalStylesUrl,
     },
     {
-      rel: "stylesheet",
+      rel: 'stylesheet',
       href: globalMediumStylesUrl,
-      media: "print, (min-width: 640px)",
+      media: 'print, (min-width: 640px)',
     },
     {
-      rel: "stylesheet",
+      rel: 'stylesheet',
       href: globalLargeStylesUrl,
-      media: "screen and (min-width: 1024px)",
+      media: 'screen and (min-width: 1024px)',
     },
   ];
 };
 
-export default function App() {
+function Document({
+  title = `Remix: So great, it's funny`,
+  children,
+}: {
+  title?: string;
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <title>Remix: So great, it's funny!</title>
+        <meta charSet='utf-8' />
+        <title>{title}</title>
         <Links />
       </head>
       <body>
-        <Outlet />
-        <LiveReload />
+        {children}
+        {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <Document title='Oh no!'>
+      <div className='error-container'>
+        <h1>Something went wrong</h1>
+        <p>{error.message}</p>
+      </div>
+    </Document>
   );
 }
--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,19 +1,17 @@
-import type { LoaderFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
-import type { Joke } from "@prisma/client";
+import type { LoaderFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { Link, useLoaderData, useParams } from '@remix-run/react';
+import type { Joke } from '@prisma/client';
 
-import { db } from "~/utils/db.server";
+import { db } from '~/utils/db.server';
 
 type LoaderData = { joke: Joke };
 
-export const loader: LoaderFunction = async ({
-  params,
-}) => {
+export const loader: LoaderFunction = async ({ params }) => {
   const joke = await db.joke.findUnique({
     where: { id: params.jokeId },
   });
-  if (!joke) throw new Error("Joke not found");
+  if (!joke) throw new Error('Joke not found');
   const data: LoaderData = { joke };
   return json(data);
 };
@@ -25,7 +23,14 @@ export default function JokeRoute() {
     <div>
       <p>Here's your hilarious joke:</p>
       <p>{data.joke.content}</p>
-      <Link to=".">{data.joke.name} Permalink</Link>
+      <Link to='.'>{data.joke.name} Permalink</Link>
     </div>
+  );
+}
+
+export function ErrorBoundary() {
+  const { jokeId } = useParams();
+  return (
+    <div className='error-container'>{`There was an error loading joke by the id ${jokeId}. Sorry.`}</div>
   );
 }
